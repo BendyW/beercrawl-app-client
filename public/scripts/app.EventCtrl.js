09c368d113a7411ad36c4df51a8985221aab2e40
Sonia Boller <soniaboller@gmail.com>
2016-11-23 14:10:06 -0600

    removed excess code for markers

--- public/scripts/app.EventCtrl.js
+++ public/scripts/app.EventCtrl.js
@@ -25,12 +25,6 @@ angular.module('beerCrawl')
                 console.log(err)
             })
         };
-        // var bars = wickerParkBars
-        // var mapCenter = wickerCenter
-
-        // document.getElementById('submit').addEventListener('click', function() {
-        //     loadMap();
-        // });
 
         var mapCenter = {
             lat: 41.8781,
@@ -49,101 +43,93 @@ angular.module('beerCrawl')
         var directionsService = new google.maps.DirectionsService;
         var directionsDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true});
 
-    $scope.reloadMap = function(){
-        // var markers = [];
-        function reloadMarkers(){
-            for (var i=0; i<markers.length; i++) {
-                markers[i].setMap(null);
-            }
-            markers = [];
-        }
-        reloadMarkers();
-        var bars, mapCenter;
-        var neighborhood = document.getElementById('neighborhood').value;
-        if ( neighborhood === 'WP'){
-            bars = wickerParkBars;
-            mapCenter = wickerParkCenter;
-        }
-        else if ( neighborhood === 'LS'){
-            bars = loganSquareBars;
-            mapCenter = loganSquareCenter;
-        }
-        else if ( neighborhood === 'LP'){
-            bars = lincolnParkBars;
-            mapCenter = lincolnParkCenter;
-        }
-        else if ( neighborhood === 'RN'){
-            bars = riverNorthBars;
-            mapCenter = riverNorthCenter;
-        }
-        else{
-            console.log('idk what happened')
-        }
-
-
-        mapOptions.zoom = 12;
-
-        var waypoints = [];
-
-        for (var j = 1; j < bars.length -1; j++){
-            waypoints.push({location: new google.maps.LatLng(bars[j].lat, bars[j].lng),
-                            stopover: true})
-        }
-        console.log(waypoints);
-
-        directionsService.route({
-            origin: new google.maps.LatLng(bars[0].lat, bars[0].lng),
-            destination: new google.maps.LatLng(bars[4].lat, bars[4].lng),
-            waypoints: waypoints,
-            optimizeWaypoints: false,
-            travelMode: 'WALKING'
-        }, function(response, status) {
-            if (status === 'OK') {
-                directionsDisplay.setDirections(response);
-                console.log(response)
+        $scope.reloadMap = function(){
+            function reloadMarkers(){
+                for (var i=0; i<markers.length; i++) {
+                    markers[i].setMap(null);
                 }
-            else {
-                window.alert('Directions request failed due to ' + status);
+                markers = [];
             }
-        });
-
-        directionsDisplay.setMap(null);
-        directionsDisplay.setMap($scope.map);
-
-        var infoWindow = new google.maps.InfoWindow();
-        var createMarker = function (locations) {
-        for (var i = 0; i < bars.length; i++) {
-            var info = locations[i];
-            var marker = new google.maps.Marker({
-                map: $scope.map,
-                position: new google.maps.LatLng(info.lat, info.lng),
-                animation: google.maps.Animation.DROP,
-                title: info.name,
-                address: info.address,
-                visible: true
-            });
-            marker.content = '<div class="infoWindowContent">' + info.challenge1 + '<br>' + info.challenge2 + '<br>' + info.challenge3 + '</div>';
-            google.maps.event.addListener(marker, 'click', function () {
-                infoWindow.setContent('<h2>' + marker.title + '</h2>' + '<h4>' + marker.address + '</h4>' + marker.content);
-                infoWindow.open($scope.map, marker);
-            });
-            markers.push(marker);
+            reloadMarkers();
+            var bars, mapCenter;
+            var neighborhood = document.getElementById('neighborhood').value;
+            if ( neighborhood === 'WP'){
+                bars = wickerParkBars;
+                mapCenter = wickerParkCenter;
             }
+            else if ( neighborhood === 'LS'){
+                bars = loganSquareBars;
+                mapCenter = loganSquareCenter;
+            }
+            else if ( neighborhood === 'LP'){
+                bars = lincolnParkBars;
+                mapCenter = lincolnParkCenter;
+            }
+            else if ( neighborhood === 'RN'){
+                bars = riverNorthBars;
+                mapCenter = riverNorthCenter;
+            }
+            else{
+                console.log('idk what happened')
+            }
+
+            mapOptions.zoom = 12;
+
+            var waypoints = [];
+
+            for (var j = 1; j < bars.length -1; j++){
+                waypoints.push({location: new google.maps.LatLng(bars[j].lat, bars[j].lng),
+                                stopover: true})
+            }
+            // console.log(waypoints);
+
+            directionsService.route({
+                origin: new google.maps.LatLng(bars[0].lat, bars[0].lng),
+                destination: new google.maps.LatLng(bars[4].lat, bars[4].lng),
+                waypoints: waypoints,
+                optimizeWaypoints: false,
+                travelMode: 'WALKING'
+            }, function(response, status) {
+                if (status === 'OK') {
+                    directionsDisplay.setDirections(response);
+                    // console.log(response)
+                    }
+                else {
+                    window.alert('Directions request failed due to ' + status);
+                }
+            });
+
+            directionsDisplay.setMap(null);
+            directionsDisplay.setMap($scope.map);
+
+            var infoWindow = new google.maps.InfoWindow();
+            var createMarker = function (locations) {
+            for (var i = 0; i < bars.length; i++) {
+                var info = locations[i];
+                var marker = new google.maps.Marker({
+                    map: $scope.map,
+                    position: new google.maps.LatLng(info.lat, info.lng),
+                    animation: google.maps.Animation.DROP,
+                    title: info.name,
+                    address: info.address
+                });
+                marker.content = '<div class="infoWindowContent">' + info.challenge1 + '<br>' + info.challenge2 + '<br>' + info.challenge3 + '</div>';
+                google.maps.event.addListener(marker, 'click', function () {
+                    infoWindow.setContent('<h2>' + marker.title + '</h2>' + '<h4>' + marker.address + '</h4>' + marker.content);
+                    infoWindow.open($scope.map, marker);
+                });
+                markers.push(marker);
+                }
+                };
+
+            createMarker(bars);
+
+            $scope.openInfoWindow = function(e, selectedMarker){
+                e.preventDefault();
+                google.maps.event.trigger(selectedMarker, 'click');
             };
 
-        createMarker(bars);
 
-        $scope.openInfoWindow = function(e, selectedMarker){
-            e.preventDefault();
-            google.maps.event.trigger(selectedMarker, 'click');
-        };
-
-        console.log(markers, 'scope markers!!!!');
-
-        for (var k = markers.length - 1; k > markers.length - 6; k--){
-        console.log(markers[k], 'marker #', k );
-        markers[k].visible = true;
-        }
-        };
+            };
 
     });
